a92d7065cb542d6774c73634be56c5dbfaaff8f6
riki-hashimoto <hashi105den@gmail.com>
2024-07-04 13:56:20 +0900

    produstlistサンプル作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    NavbarComponent
+    NavbarComponent,
+    ProductListComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
